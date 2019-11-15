538085d2a8b663b97f5977bd32cb14a9679482f7
Emiliano Molinaro <emil.molinaro@gmail.com>
2019-11-15 18:20:25 +0100

    remove unused class PColors

--- tools.py
+++ tools.py
@@ -1,13 +1,3 @@
-class PColors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-
 class Metadata:
     name = 'HABITUS'
     version = '1.9.2'
